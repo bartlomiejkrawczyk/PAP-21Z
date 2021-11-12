b450af796dbd10d0dc666a1d93ae760b4cd4f98b
Bartomiej Krawczyk <01158771@pw.edu.pl>
2021-11-12 15:48:48 +0100

    Added basic script to generate insert statements

---
+++ generate_insertions.py
@@ -0,0 +1,137 @@
+from typing import List, Callable, Dict, Tuple
+from random import randint, choice
+
+
+EMPLOYEE_KINDS = ['waiter', 'cook']
+
+
+def insert_employee_kinds() -> List[str]:
+
+    return ['INSERT INTO employee_kinds VALUES '
+            f'({i}, {kind});\n'
+            for i, kind in enumerate(EMPLOYEE_KINDS, start=1)]
+
+
+EMPLOYEES = 10
+
+FIRST_NAMES = ['Adam', 'Bartlomiej', 'Kamil', 'Karol']
+LAST_NAMES = ['Krawczyk', 'Sudol', 'Sulkowski', 'Rogozinski']
+
+
+def insert_employees() -> List[str]:
+
+    return ['INSERT INTO employees VALUES '
+            f'({i}, {choice(FIRST_NAMES)}, {choice(LAST_NAMES)}, {randint(1, len(EMPLOYEE_KINDS))});\n'
+            for i in range(1, EMPLOYEES + 1)]
+
+
+TABLES = ['pod oknem', 'przy drzwiach', 'przy barze']
+
+
+def insert_tables() -> List[str]:
+
+    return ['INSERT INTO tables VALUES '
+            f'({i}, {name});\n'
+            for i, name in enumerate(TABLES, start=1)]
+
+
+PRODUCTS: Dict[str, List[Tuple[str, int, int, str]]] = {
+    'Drinks': [
+        ('Coca-Cola 500ml', 0, 100, 'bottle'),
+        ('Coca-Cola 1l', 0, 50, 'bottle'),
+        ('Sprite 500ml', 0, 50, 'bottle'),
+        ('Fanta 500ml', 0, 50, 'bottle')
+    ],
+    'Meat': [
+        ('Chicken Breasts', 0, 10000, 'g'),
+        ('Chicken Wings', 0, 10000, 'g'),
+        ('Beef', 0, 10000, 'g'),
+        ('Pork Chop', 0, 10000, 'g')
+    ],
+    'Vegetables': [
+        ('Onions', 0, 1000, 'g'),
+        ('Pepper', 0, 1000, 'g'),
+        ('Carrot', 0, 1000, 'g')
+    ],
+    'Fruits': [
+        ('Apple', 0, 100, 'fruit')
+    ]
+}
+
+
+def insert_product_categories() -> List[str]:
+    return ['INSERT INTO product_categories VALUES '
+            f'({i}, {name});\n'
+            for i, name in enumerate(PRODUCTS.keys(), start=1)]
+
+
+def insert_products() -> List[str]:
+    lines = []
+    for i, products in enumerate(PRODUCTS.values(), start=1):
+        lines += ['INSERT INTO products VALUES '
+                  f'({i}, {product[0]}, {product[1]}, {product[2]}, {product[3]});\n'
+                  for product in products]
+    return lines
+
+
+def insert_dish_categories() -> List[str]:
+    return []
+
+
+def insert_dishes() -> List[str]:
+    return []
+
+
+def insert_ingredients() -> List[str]:
+    return []
+
+
+def insert_recipes() -> List[str]:
+    return []
+
+
+def insert_receipts() -> List[str]:
+    return []
+
+
+def insert_orders() -> List[str]:
+    return []
+
+
+def insert_special_requests() -> List[str]:
+    return []
+
+
+def write_lines(lines: List[str]) -> None:
+    lines.append("COMMIT;\n")
+    with open('insert.sql', 'w') as handle:
+        handle.writelines(lines)
+
+
+FUNCTIONS: List[Callable[[], List[str]]] = [
+    insert_employee_kinds,
+    insert_employees,
+    insert_tables,
+    insert_product_categories,
+    insert_products,
+    insert_dish_categories,
+    insert_dishes,
+    insert_ingredients,
+    insert_recipes,
+    insert_receipts,
+    insert_orders,
+    insert_special_requests
+]
+
+
+def main():
+    lines: List[str] = []
+    for func in FUNCTIONS:
+        lines += func()
+        lines.append('\n')
+
+    write_lines(lines)
+
+
+if __name__ == '__main__':
+    main()
---
+++ insert.sql
@@ -0,0 +1,44 @@
+INSERT INTO employee_kinds VALUES (1, waiter);
+INSERT INTO employee_kinds VALUES (2, cook);
+
+INSERT INTO employees VALUES (1, Adam, Sulkowski, 2);
+INSERT INTO employees VALUES (2, Karol, Rogozinski, 1);
+INSERT INTO employees VALUES (3, Kamil, Sulkowski, 1);
+INSERT INTO employees VALUES (4, Adam, Krawczyk, 1);
+INSERT INTO employees VALUES (5, Kamil, Sudol, 1);
+INSERT INTO employees VALUES (6, Adam, Sudol, 2);
+INSERT INTO employees VALUES (7, Kamil, Krawczyk, 2);
+INSERT INTO employees VALUES (8, Bartlomiej, Krawczyk, 1);
+INSERT INTO employees VALUES (9, Karol, Sudol, 2);
+INSERT INTO employees VALUES (10, Bartlomiej, Krawczyk, 1);
+
+INSERT INTO tables VALUES (1, pod oknem);
+INSERT INTO tables VALUES (2, przy drzwiach);
+INSERT INTO tables VALUES (3, przy barze);
+
+INSERT INTO product_categories VALUES (1, Drinks);
+INSERT INTO product_categories VALUES (2, Meat);
+INSERT INTO product_categories VALUES (3, Vegetables);
+INSERT INTO product_categories VALUES (4, Fruits);
+
+INSERT INTO products VALUES (1, Coca-Cola 500ml, 0, 100, bottle);
+INSERT INTO products VALUES (1, Coca-Cola 1l, 0, 50, bottle);
+INSERT INTO products VALUES (1, Sprite 500ml, 0, 50, bottle);
+INSERT INTO products VALUES (1, Fanta 500ml, 0, 50, bottle);
+INSERT INTO products VALUES (2, Chicken Breasts, 0, 10000, g);
+INSERT INTO products VALUES (2, Chicken Wings, 0, 10000, g);
+INSERT INTO products VALUES (2, Beef, 0, 10000, g);
+INSERT INTO products VALUES (2, Pork Chop, 0, 10000, g);
+INSERT INTO products VALUES (3, Onions, 0, 1000, g);
+INSERT INTO products VALUES (3, Pepper, 0, 1000, g);
+INSERT INTO products VALUES (3, Carrot, 0, 1000, g);
+INSERT INTO products VALUES (4, Apple, 0, 100, fruit);
+
+
+
+
+
+
+
+
+COMMIT;
