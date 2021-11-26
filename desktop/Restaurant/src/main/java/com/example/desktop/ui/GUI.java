bbf534beef224fde613856d17b6b001b3c3a694e
Kamil Sulkowski <01158938@pw.edu.pl>
2021-11-26 07:09:43 +0100

    Change strings list to frames list. Refactor code.

--- desktop/Restaurant/src/main/java/com/example/desktop/ui/GUI.java
+++ desktop/Restaurant/src/main/java/com/example/desktop/ui/GUI.java
@@ -43,51 +43,46 @@ public class GUI {
 
         panelTop.setBorder(blackline);
         panelTop.setPreferredSize(new Dimension(800, 25));
-
-//        panelLeft.setBackground(Color.GREEN);
-//        panelRight.setBackground(Color.YELLOW);
         panelTop.setBackground(Color.GRAY);
 
-//        Vector<JPanel> employees = new Vector<JPanel>();
+        JPanel panelLeftTitle = new JPanel();
+        panelLeftTitle.setBorder(blackline);
+        panelLeftTitle.setBackground(Color.DARK_GRAY);
 
-        Vector<JLabel> employees = new Vector<JLabel>();
+        JLabel panelLeftTitleText = new JLabel("Orders placed:");
+        panelLeftTitleText.setForeground(Color.WHITE);
+
+        panelLeftTitle.add(panelLeftTitleText);
+        panelLeft.add(panelLeftTitle);
+
+        JPanel scrollablePanel = new JPanel();
+        scrollablePanel.setLayout(new BoxLayout(scrollablePanel, BoxLayout.Y_AXIS));
+        Vector<JPanel> employees = new Vector<JPanel>();
 
         for (int i = 0; i < 4; ++i) {
             for (Employee employee : App.getEmployeesFromApi()) {
-//                JPanel employeePanel = new JPanel();
-//                employeePanel.setLayout(new FlowLayout());
-//                employeePanel.setBorder(blackline);
-//                employeePanel.setPreferredSize(new Dimension(200, 30));
-                employees.add(new JLabel(" " + employee.getId().toString() + "\t" + employee.getFirstName() + "\t" + employee.getFamilyName() + "\t" + employee.getEmployeeKindId().toString()));
-//                JLabel id = new JLabel(employee.getId().toString());
-//                JLabel firstName = new JLabel(employee.getFirstName());
-//                JLabel familyName = new JLabel(employee.getFamilyName());
-//                JLabel employeeKindId = new JLabel(employee.getEmployeeKindId().toString());
-//                employeePanel.add(id);
-//                employeePanel.add(firstName);
-//                employeePanel.add(familyName);
-//                employeePanel.add(employeeKindId);
-//                employees.add(employeePanel);
+                JPanel employeePanel = new JPanel();
+                employeePanel.setBorder(blackline);
+                employeePanel.setPreferredSize(new Dimension(200, 30));
+                JLabel id = new JLabel(employee.getId().toString());
+                JLabel firstName = new JLabel(employee.getFirstName());
+                JLabel familyName = new JLabel(employee.getFamilyName());
+                JLabel employeeKindId = new JLabel(employee.getEmployeeKindId().toString());
+                employeePanel.add(id);
+                employeePanel.add(firstName);
+                employeePanel.add(familyName);
+                employeePanel.add(employeeKindId);
+                employees.add(employeePanel);
             }
         }
 
-        String list = new String();
-        for (JLabel employee: employees) {
-            list += employee.getText();
-            list += '\n';
-        }
+        for(JPanel employee: employees) scrollablePanel.add(employee);
 
-        JTextArea area = new JTextArea(list);
-//        scrollablePanel.setLayout(new BoxLayout(panelLeft, BoxLayout.Y_AXIS));
+        JScrollPane scrollFrame = new JScrollPane(scrollablePanel);
+        scrollablePanel.setAutoscrolls(true);
+        scrollFrame.setPreferredSize(new Dimension(400, 350));
 
-//        for(JPanel employee: employees) scrollablePanel.add(employee);
-
-        JScrollPane scroll = new JScrollPane(area);
-        panelLeft.add(scroll);
-//        JScrollPane scroll = new JScrollPane(panelLeft);
-//        scroll.setViewportView(employees);
-//        employees.setLayoutOrientation(JList.VERTICAL);
-//        panelLeft.add(scroll);
+        panelLeft.add(scrollFrame);
 
         frame.add(panelLeft, BorderLayout.LINE_START);
         frame.add(panelRight, BorderLayout.LINE_END);
@@ -95,39 +90,4 @@ public class GUI {
         frame.pack();
         frame.setVisible(true);
     }
-
-//    private static void getListOfOrders() {
-//        Call<List<Order>> call = App.interfaceApi.getOrders();
-//        call.enqueue(new Callback<String>() {
-//            @Override
-//            public void onResponse(@NotNull Call<String> call, @NotNull Response<String> response) {
-//                if (response.isSuccessful() && response.body() != null) {
-//                    panelLeft
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(@NotNull Call<String> call, @NotNull Throwable throwable) {
-//
-//            }
-//        });
-//    }
-
-    private static void getHelloWorld(JButton button) {
-        Call<String> call = App.interfaceApi.getHelloWorld();
-        call.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(@NotNull Call<String> call, @NotNull Response<String> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    button.setText(response.body());
-                }
-            }
-
-            @Override
-            public void onFailure(@NotNull Call<String> call, @NotNull Throwable throwable) {
-
-            }
-        });
-    }
-
 }
