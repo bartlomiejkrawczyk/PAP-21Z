7fc6889cfc63bfc37c7263281f8a89b2c7b38fec
krogozin <01158884@pw.edu.pl>
2021-12-01 01:41:01 +0100

    Added Cook entity

---
+++ desktop/Restaurant/src/main/java/com/example/desktop/entities/Cook.java
@@ -0,0 +1,21 @@
+package com.example.desktop.entities;
+
+public class Cook {
+    private Long id;
+    private String name;
+
+    public Cook(String name) {
+        this.name = name;
+    }
+
+    public Cook (Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() { return this.id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+}
