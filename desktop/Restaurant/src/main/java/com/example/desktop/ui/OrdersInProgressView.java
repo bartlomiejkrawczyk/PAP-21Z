df2259d12d3425afd64bb01d7b8464699fa3c90e
asudol <01158935@pw.edu.pl>
2022-01-07 18:56:21 +0100

    Completed OrdersInProgressView

--- desktop/Restaurant/src/main/java/com/example/desktop/ui/OrdersInProgressView.java
+++ desktop/Restaurant/src/main/java/com/example/desktop/ui/OrdersInProgressView.java
@@ -1,4 +1,64 @@
 package com.example.desktop.ui;
 
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
 public class OrdersInProgressView {
+
+    private JPanel panelRight;
+    private JPanel scrollablePanelRight;
+    private JPanel panelRightTitle;
+
+    private JScrollPane scrollFrameRight;
+    private JLabel panelRightTitleText;
+
+    private Border blackLine;
+
+
+    public OrdersInProgressView(){
+        panelRight = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        scrollablePanelRight = new JPanel();
+        scrollFrameRight = new JScrollPane(scrollablePanelRight);
+        panelRightTitle = new JPanel();
+        panelRightTitleText = new JLabel("Orders in progress:");
+        blackLine = BorderFactory.createLineBorder(Color.black);
+
+        panelRightTitleText.setForeground(Color.WHITE);
+
+        panelRightTitle.setBorder(blackLine);
+        panelRightTitle.setBackground(Color.DARK_GRAY);
+        panelRightTitle.add(panelRightTitleText);
+
+        scrollablePanelRight.setLayout(new BoxLayout(scrollablePanelRight, BoxLayout.Y_AXIS));
+        scrollablePanelRight.setAutoscrolls(true);
+        scrollFrameRight.setPreferredSize(new Dimension(400, 350));
+
+        panelRight.setBorder(blackLine);
+        panelRight.setPreferredSize(new Dimension(400, 150));
+        panelRight.setLayout(new BoxLayout(panelRight, BoxLayout.Y_AXIS));
+        panelRight.add(panelRightTitle);
+        panelRight.add(scrollFrameRight);
+
+    }
+
+    public JPanel getPanelRight() {
+        return panelRight;
+    }
+
+    public JPanel getScrollablePanelRight() {
+        return scrollablePanelRight;
+    }
+
+    public JPanel getPanelRightTitle() {
+        return panelRightTitle;
+    }
+
+    public JScrollPane getScrollFrameRight() {
+        return scrollFrameRight;
+    }
+
+    public JLabel getPanelRightTitleText() {
+        return panelRightTitleText;
+    }
 }
