172ffd80d645a0cb3c073be924c10bc0253b4024
asudol <01158935@pw.edu.pl>
2022-01-07 20:05:45 +0100

    Unified *ItemViews to one ItemView

---
+++ desktop/Restaurant/src/main/java/com/example/desktop/ui/ItemView.java
@@ -0,0 +1,71 @@
+package com.example.desktop.ui;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class ItemView {
+
+    private JPanel panel;
+    private JLabel label;
+    private JButton button1;
+    private JButton button2;
+
+    public ItemView(String buttonName) {
+
+        Border blackLine = BorderFactory.createLineBorder(Color.black);
+
+        panel = new JPanel();
+        panel.setBorder(blackLine);
+        panel.setPreferredSize(new Dimension(200, 28));
+        panel.setLayout(null);
+
+        label = new JLabel();
+        label.setBounds(25, 0, 250, 28);
+
+        button1 = new JButton(buttonName);
+        button1.setBounds(200, 0, 90, 28);
+
+        panel.add(label);
+        panel.add(button1);
+    }
+
+    public ItemView(String firstButtonName, String secondButtonName) {
+
+        Border blackLine = BorderFactory.createLineBorder(Color.black);
+
+        panel = new JPanel();
+        panel.setBorder(blackLine);
+        panel.setPreferredSize(new Dimension(200, 28));
+        panel.setLayout(null);
+
+        label = new JLabel();
+        label.setBounds(25, 0, 250, 28);
+
+        button1 = new JButton(firstButtonName);
+        button1.setBounds(200, 0, 90, 28);
+
+        button2 = new JButton(secondButtonName);
+        button2.setBounds(290, 0, 90, 28);
+
+        panel.add(label);
+        panel.add(button1);
+        panel.add(button2);
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+
+    public JLabel getLabel() {
+        return label;
+    }
+
+    public JButton getButton1() {
+        return button1;
+    }
+
+    public JButton getButton2() {
+        return button2;
+    }
+}
