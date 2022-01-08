968d974e028469fe1f319cd1aa17563c1b5b2147
asudol <01158935@pw.edu.pl>
2022-01-08 21:10:14 +0100

    Added initials to OrderInProgressItem label

--- desktop/Restaurant/src/main/java/com/example/desktop/controllers/OrderInProgressItemController.java
+++ desktop/Restaurant/src/main/java/com/example/desktop/controllers/OrderInProgressItemController.java
@@ -2,6 +2,7 @@ package com.example.desktop.controllers;
 
 import com.example.desktop.App;
 import com.example.desktop.AppDatabase;
+import com.example.desktop.entities.Employee;
 import com.example.desktop.entities.Order;
 import com.example.desktop.ui.DetailsView;
 import com.example.desktop.ui.ItemView;
@@ -23,7 +24,51 @@ public class OrderInProgressItemController {
     }
 
     private void updateView() {
-        this.view.getLabel().setText(orderInProgress.getDish().getName());
+        String label;
+        label = orderInProgress.getDish().getName();
+        label += "  (:";
+        label += getInitials();
+        label += ") ";
+        this.view.getLabel().setText(label);
+    }
+
+    private String getInitials(){
+        StringBuilder initials = new StringBuilder();
+        int nameI, surnameI;
+
+        Employee cook = db.getEmployeeById(orderInProgress.getEmployee().getId());
+        char[] nameChars = cook.getFirstName().toCharArray();
+        char[] surnameChars = cook.getFamilyName().toCharArray();
+
+        if (!(nameChars.length > 2)){
+            nameI = nameChars.length;
+        } else {
+            nameI = 3;
+        }
+
+        if (!(surnameChars.length > 2)){
+            surnameI = surnameChars.length;
+        } else {
+            surnameI = 3;
+        }
+
+        for (char ch: nameChars){
+            initials.append(ch);
+            nameI -= 1;
+            if (nameI < 1){
+                break;
+            }
+        }
+
+        for (char ch: surnameChars){
+            initials.append(ch);
+            surnameI -= 1;
+            if (surnameI < 1){
+                break;
+            }
+        }
+
+        return initials.toString();
     }
 
     private void updateActionListener(){
