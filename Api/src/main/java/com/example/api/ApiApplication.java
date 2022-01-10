e727b1eba0cb314332f91c05010cf06343b5c2be
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-01-10 18:58:34 +0100

    Removed unused methods

--- Api/src/main/java/com/example/api/ApiApplication.java
+++ Api/src/main/java/com/example/api/ApiApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableJpaRepositories("com.example.api.repositories")
-//@EntityScan("com.example.api.entities")
 public class ApiApplication {
 
     public static void main(String[] args) {
