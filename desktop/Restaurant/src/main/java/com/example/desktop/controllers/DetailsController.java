e6859703d457e21dd44d358bfc8273b3a4c3c0f7
krogozin <01158884@pw.edu.pl>
2022-01-10 01:05:20 +0100

    More details in details

--- desktop/Restaurant/src/main/java/com/example/desktop/controllers/DetailsController.java
+++ desktop/Restaurant/src/main/java/com/example/desktop/controllers/DetailsController.java
@@ -50,8 +50,8 @@ public class DetailsController {
     private void addDetails(){
         String name;
         String requests = new String(" ");
-        String recipe = new String(" ");
-        String ingredients = new String(" ");
+        StringBuilder recipe = new StringBuilder(new String(" "));
+        StringBuilder ingredients = new StringBuilder(new String(" "));
         String imagePath = new String(" ");
 
         Product product;
@@ -64,27 +64,32 @@ public class DetailsController {
             requests += req.getRequest() + "\n\t";
         }
 
+        int step = 1;
         for (Recipe rec: dish.getRecipes()){
-            recipe += rec.getRecipe() + "\n\t";
+            recipe.append(step).append(". ");
+            recipe.append(rec.getRecipe()).append("\n\t");
+            step++;
         }
 
         for (Ingredient ingr: dish.getIngredients()){
             product = db.getProductFromIngredient(ingr);
             if (!product.getName().isEmpty()){
-                ingredients += product.getName() + "\n\t";
+                ingredients.append(ingr.getQuantity()).append(" ");
+                ingredients.append(product.getUnit()).append(" ");
+                ingredients.append(product.getName()).append("\n\t");
             }
         }
 
         if (name.isEmpty() || name.equals(" ")) name = "null \n";
         if (requests.isEmpty() || requests.equals(" ")) requests = "null \n";
-        if (recipe.isEmpty() || recipe.equals(" ")) recipe = "null \n";
-        if (ingredients.isEmpty() || ingredients.equals(" ")) ingredients = "null \n";
+        if ((recipe.length() == 0) || recipe.toString().equals(" ")) recipe = new StringBuilder("null \n");
+        if ((ingredients.length() == 0) || ingredients.toString().equals(" ")) ingredients = new StringBuilder("null \n");
         if (imagePath.isEmpty() || imagePath.equals(" ")) imagePath = "null \n";
 
         details += "Name: " + name + "\n\n";
         details += "Special requests: " + requests + "\n";
-        details += "Recipe: " + recipe + "\n";
-        details += "Ingredients: " + ingredients + "\n";
+        details += "Recipe:           " + recipe + "\n";
+        details += "Ingredients:    " + ingredients + "\n";
 
         addImage(imagePath, view.getLabel());
         if (view.getLabel().getIcon() != null) {
