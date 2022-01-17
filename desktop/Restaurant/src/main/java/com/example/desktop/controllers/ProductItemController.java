899f940e3bf7f9248efda06cff44449e82156d8e
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-01-17 12:24:42 +0100

    Resetting input text field after update

--- desktop/Restaurant/src/main/java/com/example/desktop/controllers/ProductItemController.java
+++ desktop/Restaurant/src/main/java/com/example/desktop/controllers/ProductItemController.java
@@ -76,5 +76,6 @@ public class ProductItemController {
                         + product.getUnit()
                         + ")"
         );
+        view.getInputTextField().setText("Enter amount");
     }
 }
